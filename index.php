f41f8c036b4466cac82b4941eeaebf1d5b1890eb
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-19 11:46:39 +0100

    exo5

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $int = 1;
+    while ($int <=15) {
+      echo "On y arrive presque" . '<br>';
+      $int++;
+    }
+    ?>
+  </body>
